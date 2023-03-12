ea58509c30b6fac1b56ac3175367a3c002145758
CarolinaVirginio <carolinavir35@gmail.com>
2023-03-12 20:24:18 -0300

    Finalizando estilização do componente Menu.

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,14 +1,14 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 import './styles.css';
 
 export default function Menu() {
     return (
         <div className="menu">
-            <Link to="/">Home</Link>
-            <Link to="/produtos">Produtos</Link>
-            <Link to="/categorias">Categorias</Link>
-            <Link to="/meus-pedidos">Meus Pedidos</Link>
+            <Link to="/" className={useLocation().pathname === '/' ? 'selected-link' : ''}>Home</Link>
+            <Link to="/produtos" className={useLocation().pathname === '/produtos' ? 'selected-link' : ''}>Produtos</Link>
+            <Link to="/categorias" className={useLocation().pathname === '/categorias' ? 'selected-link' : ''}>Categorias</Link>
+            <Link to="/meus-pedidos" className={useLocation().pathname === '/meus-pedidos' ? 'selected-link' : ''}>Meus Pedidos</Link>
         </div>
     )
 }
